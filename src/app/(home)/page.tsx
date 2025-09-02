c8903b5222d7421eb981c2d49fc004d43244d27b
patryk <paniczgg@gmail.com>
2025-09-02 05:16:46 +0200

    home layout

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+import { HomeView } from "@/modules/home/ui/views/home-view";
+
+export default function Home() {
+  return (
+    <HomeView />
+  );
+}
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,13 +1,13 @@
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
 
-export default function Home() {
+export const HomeView = () => {
   return (
-      <div className="p-4">
-    <div className="flex flex-col gap-y-4">
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
         <div>
           <Button variant="elevated">
             I am a button
@@ -27,5 +27,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-  );
+  )
 }
