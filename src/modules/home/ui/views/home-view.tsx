0603c942ea929fccf77c707e62a5f5cdbd66907a
patryk <paniczgg@gmail.com>
2025-09-02 10:20:19 +0200

    payload-config

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,31 +1,26 @@
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
 
-export const HomeView = () => {
+export const HomeView = async () => {
+  const payload = await getPayload({
+    config: configPromise
+  })
+
+  const data = await payload.find({ 
+    collection: "categories"
+  })
+
   return (
-    <div className="p-4">
-      <div className="flex flex-col gap-y-4">
-        <div>
-          <Button variant="elevated">
-            I am a button
-          </Button>
-        </div>
-        <div>
-          <Input placeholder="I am an input" />
-        </div>
-        <div>
-          <Progress value={50} />
-        </div>
-        <div>
-          <Textarea placeholder="I am a Textarea" />
-        </div>
-        <div>
-          <Checkbox />
-        </div>
-      </div>
+    <div>
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
     </div>
   )
 }
