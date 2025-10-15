0eeb23a05344a5f689cfdf114c5dff2f8aee3d59
patryk <paniczgg@gmail.com>
2025-10-15 08:55:16 +0200

    deployment fix 2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,18 +6,18 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantUrl(tenantSlug: string) {
-  const isDevelopment = process.env.NODE_ENV === 'development'
+  const isDevelopment = process.env.NODE_ENV === "development";
   const isSubdomainRoutingEnabled =
-    Boolean(process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING!)
+    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING! === "true";
 
   if (isDevelopment || !isSubdomainRoutingEnabled) {
     return `${process.env.NEXT_PUBLIC_APP_URL!}/tenants/${tenantSlug}`;
   }
 
-  const protocol = "https"
+  const protocol = "https";
   const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
 
-  return `${protocol}://${tenantSlug}.${domain}`
+  return `${protocol}://${tenantSlug}.${domain}`;
 }
 
 export function formatCurrency(value: number | string) {
